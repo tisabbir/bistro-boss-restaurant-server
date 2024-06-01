d5880c4b5b396e888fa8cd79542e09023205b365
TI Sabbir <tisabbir0@gmail.com>
2024-06-01 16:56:26 +0600

    Now menu can be updated

--- index.js
+++ index.js
@@ -134,10 +134,34 @@ async function run() {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
+    
+    app.get('/menu/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await menuCollection.findOne(query)
+      res.send(result);
+    })
 
     app.post('/menu', verifyToken, verifyAdmin, async(req, res)=>{
       const item = req.body;
       const result = await menuCollection.insertOne(item);
+      res.send(result);
+    })
+    app.patch('/menu/:id', async(req,res)=>{
+      const item = req.body;
+      const id = req.params.id;
+      const filter = {_id : new ObjectId(id)}
+      const updatedDoc = {
+        $set : {
+          name: item.name,
+          category : item.category,
+          recipe : item.recipe, 
+          image : item.image,
+          price : item.price,
+        }
+      }
+
+      const result = await menuCollection.updateOne(filter, updatedDoc);
       res.send(result);
     })
 
